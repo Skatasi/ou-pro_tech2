41e00f03c030d20362fcf8de45a1bb8b11943b5b
Skatasi <wantainuinu11@gmail.com>
2023-07-27 22:25:28 +0900

    add kadai11

--- kadai11/main.py
+++ kadai11/main.py
@@ -11,6 +11,45 @@ import sklearn.datasets
 import matplotlib.pylab as plt
 import sklearn.model_selection
 import sklearn.svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_validate
+from PIL import Image, ImageOps
+
+def class_average(X, y, img_size):
+    A = []
+    for i in range(10):
+        x = X[y == i]
+        x = x.mean(0)
+        A.append(x.reshape(img_size))
+    return A
+
+def cross_validate(clf_type, X, y, cv):
+    train_scores = []
+    test_scores = []
+
+    if clf_type == 'kNN':
+        clf = sklearn.neighbors.KNeighborsClassifier()
+    if clf_type == 'MLP':
+        clf = sklearn.neural_network.MLPClassifier()
+    if clf_type == 'NaiveBayes':
+        clf = sklearn.naive_bayes.GaussianNB()
+    if clf_type == 'SVM':
+        clf = sklearn.svm.SVC()
+    scores = sklearn.model_selection.cross_validate(clf, X, y, cv=cv, return_train_score=True)
+    
+    return scores['train_score'],scores['test_score']
+
+def load_img(test_img_number):
+    img = Image.open(str(test_img_number)+'.jpg')
+    img_gray = img.convert('L')
+    img_invert = ImageOps.invert(img_gray)
+    img_resize = img_invert.resize(img_size)
+    img_array = np.array(img_resize)/16
+    img_reshape = img_array.reshape([-1, np.prod(img_size)])
+    return np.array(img_reshape[0])
 
 if __name__=="__main__":
     np.random.seed(0)
@@ -25,7 +64,6 @@ if __name__=="__main__":
     class_labels = np.unique(y)
     n_classes = len(class_labels)
     img_size = [8, 8]
-    print(X)
     ave_imgs = class_average(X, y, img_size) # output matrix (n_samples x 8 x 8)    
     plt.figure(1); plt.clf()
     for cc, class_label in enumerate(class_labels):
@@ -60,12 +98,5 @@ if __name__=="__main__":
         
         y_est = clf.predict(test_x[np.newaxis])
         print('Test class {} image --> classified into class {}.'.format(cc, y_est[0]))
+    plt.show()
 
-def class_average(X, y, img_size):
-    return
-
-def cross_validate(clf_type, X, y, cv=n_fold):
-    return
-
-def load_img(test_img_number):
-    return
